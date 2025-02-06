291c43ec380bc9fc877787282ef53f347edd2a61
Manav Suryavanshi <manavsuryawanshi1234@gmail.com>
2025-02-06 19:41:52 +0530

    modal hero hide issue fixed

--- src/components/EnquireNowButton.jsx
+++ src/components/EnquireNowButton.jsx
@@ -14,7 +14,7 @@ const EnquireNowButton = () => {
         <>
             <button
                 onClick={() => setModalIsOpen(true)}
-                className="fixed right-0 top-1/2 z-50 transform -translate-y-1/2 bg-[#1e5593] text-white px-3.5 sm:px-4 text-sm sm:text-base py-3 rounded shadow-lg cursor-pointer"
+                className="fixed right-0 top-1/2 z-40 transform -translate-y-1/2 bg-[#1e5593] text-white px-3.5 sm:px-4 text-sm sm:text-base py-3 rounded shadow-lg cursor-pointer"
             >
                 <div className="flex flex-col -space-y-0.5">
                     <p>E</p>
@@ -33,7 +33,7 @@ const EnquireNowButton = () => {
                 isOpen={modalIsOpen}
                 onRequestClose={() => setModalIsOpen(false)}
                 className="bg-white px-6 py-6 rounded shadow-lg max-w-xl w-full mx-auto relative z-50"
-                overlayClassName="fixed inset-0 bg-[rgba(0,0,0,0.5)] flex items-center justify-center p-4"
+                overlayClassName="fixed inset-0 bg-[rgba(0,0,0,0.5)] flex items-center justify-center p-4 z-50"
             >
                 <button onClick={() => setModalIsOpen(false)} className="absolute top-3 right-3 cursor-pointer"><IoClose className="h-7 w-7 text-gray-500 flex-shrink-0" /></button>
                 <form>
